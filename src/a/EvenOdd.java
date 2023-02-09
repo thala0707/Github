5f046be1643871cdbd5fab13b747dde21b530289
shrey <shrey@SHREYA>
2023-02-09 16:33:06 +0530

    logical even odd

---
+++ src/a/EvenOdd.java
@@ -0,0 +1,16 @@
+package a;
+
+public class EvenOdd {
+	public static void main(String[] args) {
+		int num=65651615;
+		if(num%2==0)     //%MEANS mod  it compares reminder value
+		{
+			System.out.println("number is EVEN");
+		}
+		else
+		{
+			System.out.println("number is ODD");
+		}
+	}
+
+}
